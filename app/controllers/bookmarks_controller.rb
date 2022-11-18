98c20f8276bdfc8b056c00f9c900843df6a8df6e
ekatsnelson <elisek222@gmail.com>
2022-11-18 22:16:58 +0000

    application controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,10 @@
 class BookmarksController < ApplicationController
   
-  before_action(:load_current_user)
+  # before_action(:load_current_user)
   
-  def load_current_user
-    @current_user = User.where({ :id => session[:user_id] }).at(0)
-  end
+  # def load_current_user
+  #   @current_user = User.where({ :id => session[:user_id] }).at(0)
+  # end
 
   def index
     #matching_bookmarks = Bookmark.where({ :user_id => session.fetch(:user_id)})
