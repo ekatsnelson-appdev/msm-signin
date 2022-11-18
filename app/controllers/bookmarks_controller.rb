72c29bdee62582cabaa330e8593d590ddb67909c
ekatsnelson <elisek222@gmail.com>
2022-11-18 22:13:07 +0000

    load current user

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,12 @@
 class BookmarksController < ApplicationController
+  def load_current_user
+    @current_user = User.where({ :id => session[:user_id] }).at(0)
+  end
+
   def index
     #matching_bookmarks = Bookmark.where({ :user_id => session.fetch(:user_id)})
 
-    @current_user = User.where({ :id => session[:user_id] }).at(0)
+    self.load_current_user
     matching_bookmarks = @current_user.bookmarks
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
@@ -11,6 +15,7 @@ class BookmarksController < ApplicationController
   end
 
   def show
+    self.load_current_user
     the_id = params.fetch("path_id")
 
     matching_bookmarks = Bookmark.where({ :id => the_id })
@@ -21,6 +26,8 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    self.load_current_user
+
     the_bookmark = Bookmark.new
     the_bookmark.user_id = session.fetch(:user_id) #params.fetch("query_user_id")
     the_bookmark.movie_id = params.fetch("query_movie_id")
@@ -34,6 +41,7 @@ class BookmarksController < ApplicationController
   end
 
   def update
+    self.load_current_user
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
@@ -49,6 +57,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    self.load_current_user
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
